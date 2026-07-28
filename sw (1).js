a8eb65be588df33b58e6784b401eb377ce2721d9
wadinastock <ilyass12bihi@gmail.com>
2026-07-28 10:25:22 +0400

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,17 @@
+const CACHE = "stock-ledger-v1";
+const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
